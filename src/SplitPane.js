fe1d848ca616f42efcc0446c27ed0212a05529f2
Ravi Bhoraskar <rav@meta.com>
2023-07-14 17:13:02 -0700

    Init

---
+++ src/SplitPane.js
@@ -0,0 +1,137 @@
+import React, {
+  createRef,
+  useContext,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
+import QuoteContext from "./QuoteContext";
+import SplitPaneContext from "./SplitPaneContext";
+
+const SplitPane = ({ children, ...props }) => {
+  const [clientHeight, setClientHeight] = useState(null);
+  const [clientWidth, setClientWidth] = useState(null);
+  const yDividerPos = useRef(null);
+  const xDividerPos = useRef(null);
+
+  const onMouseHoldDown = (e) => {
+    yDividerPos.current = e.clientY;
+    xDividerPos.current = e.clientX;
+  };
+
+  const onMouseHoldUp = () => {
+    yDividerPos.current = null;
+    xDividerPos.current = null;
+  };
+
+  const onMouseHoldMove = (e) => {
+    if (!yDividerPos.current && !xDividerPos.current) {
+      return;
+    }
+
+    setClientHeight(clientHeight + e.clientY - yDividerPos.current);
+    setClientWidth(clientWidth + e.clientX - xDividerPos.current);
+
+    yDividerPos.current = e.clientY;
+    xDividerPos.current = e.clientX;
+  };
+
+  useEffect(() => {
+    document.addEventListener("mouseup", onMouseHoldUp);
+    document.addEventListener("mousemove", onMouseHoldMove);
+
+    return () => {
+      document.removeEventListener("mouseup", onMouseHoldUp);
+      document.removeEventListener("mousemove", onMouseHoldMove);
+    };
+  });
+
+  return (
+    <div {...props}>
+      <SplitPaneContext.Provider
+        value={{
+          clientHeight,
+          setClientHeight,
+          clientWidth,
+          setClientWidth,
+          onMouseHoldDown,
+        }}
+      >
+        {children}
+      </SplitPaneContext.Provider>
+    </div>
+  );
+};
+
+export const Divider = (props) => {
+  const { onMouseHoldDown } = useContext(SplitPaneContext);
+
+  return <div {...props} onMouseDown={onMouseHoldDown} />;
+};
+
+export const SplitPaneTop = (props) => {
+  const topRef = createRef();
+  const { clientHeight, setClientHeight } = useContext(SplitPaneContext);
+  const { quotes, setCurrQuote, updateFrame } = useContext(QuoteContext);
+
+  useEffect(() => {
+    if (!clientHeight) {
+      setClientHeight(topRef.current.clientHeight);
+      return;
+    }
+
+    topRef.current.style.minHeight = clientHeight + "px";
+    topRef.current.style.maxHeight = clientHeight + "px";
+  }, [clientHeight]);
+
+  return (
+    <div {...props} className="split-pane-top" ref={topRef}>
+      <div>
+      <input type="text" placeholder="What should I change?"/>
+      </div>
+      <div>
+      <input type="button" value="Update Frame" onClick={updateFrame}/>
+      </div>
+    </div>
+  );
+};
+
+export const SplitPaneBottom = (props) => {
+  const { innerHTML } = useContext(QuoteContext);
+
+  return (
+    <div {...props} className="split-pane-bottom">
+      <b>Current HTML:<br/></b> {innerHTML}
+    </div>
+  );
+};
+
+export const SplitPaneLeft = (props) => {
+  const topRef = createRef();
+  const { clientWidth, setClientWidth } = useContext(SplitPaneContext);
+
+  useEffect(() => {
+    if (!clientWidth) {
+      setClientWidth(topRef.current.clientWidth / 2);
+      return;
+    }
+
+    topRef.current.style.minWidth = clientWidth + "px";
+    topRef.current.style.maxWidth = clientWidth + "px";
+  }, [clientWidth]);
+
+  return <div {...props} className="split-pane-left" ref={topRef} />;
+};
+
+export const SplitPaneRight = (props) => {
+  const { innerHTML } = useContext(QuoteContext);
+  console.log(innerHTML)
+  const markup={ __html: innerHTML};
+  return (
+    <div {...props} className="split-pane-right">
+      <div className="quote" dangerouslySetInnerHTML={markup}/>
+    </div>
+  );
+};
+
+export default SplitPane;
